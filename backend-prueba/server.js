4ed895e0d8458d341898a7255468026722f01df6
Carlos M <carmunozmar@gmail.com>
2025-08-13 15:07:22 +0200

    Prueba lanzamiento backend

--- backend-prueba/server.js
+++ backend-prueba/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config({ path: '/etc/secrets/.env' });
+require('dotenv').config();
 const connectDB = require('./config/db');
 
 // Inicializar app
